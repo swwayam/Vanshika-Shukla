1d85d538d99d1cae85e66e820bc4f8c08cb2b91a
swwayam <codingswayam24@gmail.com>
2023-06-03 09:04:57 +0530

    s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("express")();
-const server = require("http").createServer(app);
+const server = require("http").createServer();
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
