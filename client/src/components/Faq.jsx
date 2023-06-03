4a47893f41a651189e7840b12135c2f0f94cb9b8
swwayam <codingswayam24@gmail.com>
2023-06-03 08:28:46 +0530

    hackathon project vanshika

---
+++ client/src/components/Faq.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Faq() {
+  return (
+    <div className="HOWTO">
+      <div className="card">
+        <div className="card-header">How to use</div>
+        <div className="card-body">
+          <p className="card-text">
+            <ul>
+              <li>This Is A Video Calling Application</li>
+              <li>You Will Have To Enter Your Name</li>
+              <li>You Can Copy Your ID Which Is Like A Phone Number</li>
+              <li>Person Who Has Your ID Now Can Paste In The ID And Call You</li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Faq;
